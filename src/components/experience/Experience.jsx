850e35e36737c3db9a1d92b33daa4c85e1eba435
Roshan-ops <60165267+Roshan-ops@users.noreply.github.com>
2023-02-22 17:11:21 +0545

    updte

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -70,7 +70,7 @@ const Experience =() =>{
                         <article className="experience_details">
                             <BsPatchCheck className='experience_details-icon'/>
                             <div>
-                            <h4>MangoDB</h4>
+                            <h4>MongoDB</h4>
                             <small className="text-light">Experienced</small>
                             </div>
 
